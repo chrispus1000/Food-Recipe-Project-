f24cf8e50d2264fb771f2e7f8453dbe56fd4eacc
chrispus1000 <minrevac@gmail.com>
2022-07-31 23:37:14 +0300

    Food Recipe Project

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -3,12 +3,14 @@ import './Components/Style.css';
 import Meal from './Components/Meal';
 
 import {Route,Routes} from 'react-router-dom';
+import Recipedata from './Components/Recipedata';
 
 function App() {
   return (
     <>
    <Routes>
       <Route  path="/" element={<Meal/>}/>
+      <Route exact path="/:MealId" element={<Recipedata/>}/>
       
       </Routes>
     </>
